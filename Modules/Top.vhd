28b8f363062c82f8ebf7acebe184b97fb9788abd
GClishe <clishegavin@gmail.com>
2026-03-20 19:52:30 -0400

    Began top module

---
+++ Modules/Top.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity Top is
+    port(
+        i_clk : in std_logic;
+        i_btnC : in std_logic;
+        i_btnT : in std_logic;
+        o_LED1 : out std_logic
+    );
+end entity Top;
+
+architecture RTL of Top is
+    signal w_debounced_btnT : std_logic;
+begin
+
+    BtnT_Debounce_Inst : entity work.Debounce
+        generic map(
+            COUNTER_SIZE => 21
+        );
+        port map(
+            i_clk => i_clk;
+            i_bouncy => i_btnT;
+            o_debounced => w_debounced_btnT
+        );
+    
+    o_LED1 <= w_debounced_btnT;
+
+end architecture RTL;
