8f94e33966c9d3c78e4505a7caa6daaf4ae21666
GClishe <clishegavin@gmail.com>
2026-03-21 00:35:22 -0400

    Started main game loop. Identified preliminary states on state machine (subject to change)

--- src/design/Game_Loop.vhd
+++ src/design/Game_Loop.vhd
@@ -2,3 +2,23 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
+entity Game_Loop is 
+port (
+    i_clk : in std_logic;
+    i_rst : in std_logic;
+);
+end entity Game_Loop;
+architecture RTL of Game_Loop is
+    type t_SM_Main is (START,           -- Start state. Wait for reset
+                       TARGET_OFF,      -- Between-state. All LEDs off as target is selected
+                       TARGET_ON,       -- Target LED turns on. Waits briefly before cycle
+                       CYLCE,           -- Main gameplay state. LEDs cycle right-to-left, waiting for player input
+                       INCR_SCORE,      -- Player hit btnC when cycle LED matches target LED. Score increments
+                       LOSE,            -- Player missed the target LED. Failure. 
+                       WIN              -- Player score reached win condition.
+                       );
+
+    signal r_LFSR_seed_ctr : unsigned(15 downto 0) := (others => '0');  -- counter used to seed the LFSR
+begin
+
+end architecture RTL;
